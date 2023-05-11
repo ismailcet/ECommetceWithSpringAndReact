5601946e11574d8a4b5a99e6892185c52c0a5dcf
ismailcet <ismailcet3@gmail.com>
2023-05-12 01:55:00 +0300

    Full Stack ECommerce App With Java Spring Boot Starter Files

---
+++ ECommerceBackend/src/main/java/com/ismailcet/ECommerceBackend/model/User.java
@@ -0,0 +1,38 @@
+package com.ismailcet.ECommerceBackend.model;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name="name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "age")
+    private Integer age;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name="type")
+    private String type = "user";
+}
